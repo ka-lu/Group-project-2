331c0cef8bf513096302bb49cf61ecc7f63906f4
ka-lu <68350905+ka-lu@users.noreply.github.com>
2023-07-12 10:36:44 -0700

    Add files via upload

--- turtles group project.py
+++ turtles group project.py
@@ -3,8 +3,17 @@
 #Date: July, 2023
 
 import turtle
+import random
 
 #draws a picture
+
+turtle.speed(10)
+
+#list of dress colours
+dressColours=["magenta","#d85c64", "purple", "light blue","light green"]
+
+#shoe colours
+shoeColours=["black","grey",'brown']
 
 #draws the ground
 def groundDrawing(x,y):
@@ -16,7 +25,7 @@ def groundDrawing(x,y):
 
 #function that draws the dress
 def draw_dress():
-    turtle.color("#d85c64")
+    turtle.color(random.choice(dressColours))
     turtle.penup()
     turtle.goto(-150,-200)
     turtle.pendown()
@@ -87,6 +96,7 @@ def nostrilDrawing(x,y):
     turtle.stamp()
 
 def earDrawing():
+    #left ear
     turtle.shape("classic")
     turtle.penup()
     turtle.goto(-70,145)
@@ -127,20 +137,129 @@ def earDrawing():
 
 def blush():
     turtle.penup()
-    turtle.goto(-50, 80)
+    turtle.goto(-50, 70)
     turtle.pendown()
     turtle.shape("circle")
-    turtle.color("#ff72c6")
+    turtle.color("#ff80cb")
     turtle.shapesize(3)
     turtle.stamp()
 
+def eye(x,y):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.shapesize(1.9)
+    turtle.color("#f48cc8")
+    turtle.stamp()
+    turtle.shapesize(1.3)
+    turtle.color("white")
+    turtle.stamp()
+    turtle.penup()
+    turtle.setheading(180)
+    turtle.forward(3)
+    turtle.left(90)
+    turtle.forward(3)
+    turtle.pendown()
+    turtle.color("black")
+    turtle.shapesize(0.6)
+    turtle.stamp()
+
+def mouth():
+    turtle.penup()
+    turtle.color("#d8448c")
+    turtle.goto(-6, 49)
+    turtle.pendown()
+    turtle.pensize(1.5)
+    turtle.setheading(286)
+    for i in range(165):
+        turtle.left(1)
+        turtle.forward(0.7)
+
+def arm():
+    #left arm
+    turtle.penup()
+    turtle.pensize(10)
+    turtle.color("#ffacdc")
+    turtle.goto(-85,-30)
+    turtle.pendown()
+    turtle.setheading(150)
+    turtle.forward(90)
+    turtle.back(35)
+    turtle.right(35)
+    turtle.forward(35)
+    turtle.back(35)
+    turtle.left(65)
+    turtle.forward(30)
+
+    #right arm
+    turtle.penup()
+    turtle.setheading(30)
+    turtle.goto(100,-30)
+    turtle.pendown()
+    turtle.forward(90)
+    turtle.back(35)
+    turtle.left(35)
+    turtle.forward(35)
+    turtle.back(35)
+    turtle.right(65)
+    turtle.forward(30)
+
+def leg(x,y):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.shape("classic")
+    turtle.color("#ffacdc")
+    turtle.pensize(10)
+    turtle.setheading(270)
+    turtle.pendown()
+    turtle.forward(47)
+    turtle.pensize(1)
+
+def shoe(x,y):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.setheading(0)
+    turtle.pendown()
+    turtle.color(random.choice(shoeColours))  
+    turtle.shape("circle")
+    turtle.shapesize(1,3)
+    turtle.stamp()
+    turtle.shape("classic")
+
+def tail():
+    turtle.penup()
+    turtle.goto(-140,-140)
+    turtle.setheading(240)
+    turtle.color("#ffacdc")
+    turtle.pendown()
+    
+    for i in range(50):
+        turtle.right(3)
+        turtle.forward(1.2)
+    turtle.right(30)
+    for i in range(60):
+        turtle.right(4)
+        turtle.forward(0.8)
+
+    for i in range(23):
+        turtle.right(1.2)
+        turtle.forward(1.45)
 
 groundDrawing(-300,-250)
+leg(-50,-195)
+leg(50,-195)
+shoe(70,-242)
+shoe(-30,-242)
 draw_dress()
 headDrawing()
 nostrilDrawing(120,185)
 nostrilDrawing(135,188)
 earDrawing()
 blush()
+eye(-12,160)
+eye(30,179)
+mouth()
+arm()
+tail()
 
 turtle.exitonclick()
